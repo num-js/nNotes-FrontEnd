10249ed7944c5ecd653870752f00cf1793723aef
Numan Ahmed <mdnmnahmed@gmail.com>
2022-11-27 00:57:27 +0530

    Add new Note functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useMemo, useState } from 'react';
 import './App.css';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import Home from './pages/Home';
+import NewNote from './pages/NewNote';
+import { useLocalStorage } from './hooks/useLocalStorage';
+
+export type Note = {
+    id: string
+} & NoteData;
+
+export type NoteData = {
+    title: string,
+    tags: Tag[]
+    description: string
+}
+
+type RawNote = {
+    id: string
+} & RawNoteData
+
+export type RawNoteData = {
+    title: string,
+    tagIds: string[]
+    description: string
+}
+
+export type Tag = {
+    id: string
+    label: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
 
-        <h1 className="text-3xl font-bold underline decoration-red-500">
-          Hello world!
-        </h1>
+    const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+    const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const navigate = useNavigate();
+
+    const noteWithTags = useMemo(() => {
+        return notes.map(note => {
+            return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+        })
+    }, [notes, tags]);
+
+    const onCreateNote = ({ tags, ...data }: NoteData) => {
+        setNotes(prevData => {
+            const noteIdRaw = Date.now();
+            const noteId = '' + noteIdRaw;
+            return [
+                ...prevData,
+                {
+                    ...data,
+                    id: noteId,
+                    tagIds: tags.map(tag => tag.id)
+                }
+            ]
+        });
+
+        navigate('/');
+    }
+
+    const onAddTag = (newTag: Tag) => {
+        setTags(prevTags => [...prevTags, newTag])
+    }
+
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/new-note' element={
+                    <NewNote
+                        onSubmit={onCreateNote}
+                        onAddTag={onAddTag}
+                        availableTags={tags}
+                    />
+                } />
+                <Route path='/:id'>
+                    <Route index element={<h1>New Show</h1>} />
+                    <Route path="edit" element={<h1>Edit Show</h1>} />
+                </Route>
+                <Route path='*' element={<Navigate to="/" />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,97 @@
+import { useState, useRef, FormEvent } from 'react'
+import CreatableReactSelect from 'react-select/creatable';
+import { Link } from 'react-router-dom'
+import { NoteData, Tag } from '../App';
+
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+    onAddTag: (data: Tag) => void
+    availableTags: Tag[]
+}
+
+export default function NewNote({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+
+    const titleRef = useRef<HTMLInputElement>(null);
+    const descriptionRef = useRef<HTMLTextAreaElement>(null);
+    const [tags, setTags] = useState<Tag[]>([]);
+
+    const saveNewNote = (event: FormEvent) => {
+        event.preventDefault();
+        onSubmit({
+            title: titleRef.current!.value,
+            description: descriptionRef.current!.value,
+            tags: tags,
+        });
+    }
+
+    return (
+        <>
+            <section className="relative text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+
+
+                    <div className="flex flex-col">
+                        <h1 className="mb-2 text-2xl font-medium text-gray-900 title-font sm:mb-0">Add New Note</h1>
+                        <div className="h-1 overflow-hidden bg-gray-200 rounded">
+                            <div className="w-40 h-full bg-indigo-500"></div>
+                        </div>
+                    </div>
+
+                    <div className="mx-auto">
+                        <form onSubmit={saveNewNote}>
+                            <div className="flex flex-wrap -m-2">
+                                <div className="w-1/2 p-2">
+                                    <div className="relative">
+                                        <label htmlFor="name" className="text-sm leading-7 text-gray-600">Title</label>
+                                        <input ref={titleRef} type="text" className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200" />
+                                    </div>
+                                </div>
+                                <div className="w-1/2 p-2">
+                                    <div className="relative">
+                                        <label htmlFor="email" className="text-sm leading-7 text-gray-600">Topics</label>
+                                        <CreatableReactSelect className="text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none"
+                                            isMulti
+                                            options={availableTags.map(tag => {
+                                                return { label: tag.label, value: tag.id }
+                                            })}
+                                            onCreateOption={label => {
+                                                const tagIdRaw = Date.now();
+                                                const tagId = '' + tagIdRaw;
+                                                const newTag = {
+                                                    id: tagId,
+                                                    label
+                                                }
+                                                onAddTag(newTag);
+                                                setTags(prevTag => [...prevTag, newTag]);
+                                            }}
+                                            value={tags.map(tag => {
+                                                return { label: tag.label, value: tag.id }
+                                            })}
+                                            onChange={tags => {
+                                                setTags(tags.map(tag => {
+                                                    return { label: tag.label, id: tag.value }
+                                                }))
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="w-full p-2">
+                                    <div className="relative">
+                                        <label htmlFor="description" className="text-sm leading-7 text-gray-600">Description</label>
+                                        <textarea id="description" ref={descriptionRef} className="w-full h-32 px-3 py-1 text-base leading-6 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none resize-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"></textarea>
+                                    </div>
+                                </div>
+                                <div className="flex w-full justify-items-center">
+                                    <Link to="..">
+                                        <button className="px-8 py-2 mr-4 text-lg text-white bg-gray-500 border-0 rounded focus:outline-none hover:bg-gray-600">Cancel</button>
+                                    </Link>
+                                    <button type="submit" className="px-8 py-2 ml-4 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
