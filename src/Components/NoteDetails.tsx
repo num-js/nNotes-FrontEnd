e560985e1fd90d52e29e132866899f1971b5eacb
Numan Ahmed <mdnmnahmed@gmail.com>
2022-12-06 22:32:36 +0530

    note detailed, UI changes

--- src/Components/NoteDetails.tsx
+++ src/Components/NoteDetails.tsx
@@ -6,10 +6,19 @@ export function NoteDetails() {
 
     return (
         <>
-            <div>
-                <h1>{note.title}</h1>
-                <ReactMarkdown>{note.description}</ReactMarkdown>
-            </div>
+            <section className="relative text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+
+                    <div className="flex justify-between">
+                        <h1 className="mb-2 text-2xl font-medium text-gray-900 title-font sm:mb-0">{note.title}</h1>
+                        <div className="flex">
+                            <button type="submit" className="px-8 py-2 mr-4 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">Edit</button>
+                            <button className="px-8 py-2 text-lg text-white bg-red-500 border-0 rounded focus:outline-none hover:bg-red-600">Delete</button>
+                        </div>
+                    </div>
+                    <ReactMarkdown>{note.description}</ReactMarkdown>
+                </div>
+            </section>
         </>
     )
 }
