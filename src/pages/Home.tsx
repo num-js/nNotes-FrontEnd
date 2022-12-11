c14ca2bf24ae2febcc25a6400ac5581f4269e535
Numan Ahmed <mdnmnahmed@gmail.com>
2022-12-11 18:33:59 +0530

    removed previous header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,21 +35,6 @@ const Home = ({ availableTags, notes }: HomeProps) => {
         <>
             <section className="relative text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-col">
-                        <div className="flex justify-between">
-                            <h1 className="mb-2 text-2xl font-medium text-gray-900 title-font sm:mb-0">Notes</h1>
-                            <div className="flex">
-                                <Link to="/new-note">
-                                    <button type="submit" className="px-8 py-2 mr-4 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">Add Note</button>
-                                </Link>
-                                <button className="px-8 py-2 text-lg text-white bg-gray-500 border-0 rounded focus:outline-none hover:bg-gray-600"
-                                >Edit Tags</button>
-                            </div>
-                        </div>
-                        <div className="h-1 overflow-hidden bg-gray-200 rounded">
-                            <div className="w-40 h-full bg-indigo-500"></div>
-                        </div>
-                    </div>
 
                     <div>
                         <div className="mx-auto">
